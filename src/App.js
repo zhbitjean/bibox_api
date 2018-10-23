e7664603d41048740e6b81d05504a61d814864a6
Bing Li <zhbitjean@gmail.com>
2018-10-23 16:04:43 -0400

    add grid

--- src/App.js
+++ src/App.js
@@ -3,25 +3,49 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  
+  constructor(props){
+    super(props);
+    this.state = {
+      items: [],
+      isLoaded: false,
+    }
+  };
+
+ 
+  componentDidMount() {
+    const proxyUrl = 'https://cors-anywhere.herokuapp.com/',
+      symbolUrl = 'http://172.16.17.62:1507/symbol_list'
+    const targetUrl = 'https://jsonplaceholder.typicode.com/users'
+
+    //fetch(proxyUrl+targetUrl)
+    fetch(symbolUrl)
+      .then(res => res.json())
+      .then(json => {
+        this.setState({
+          isLoaded: true,
+          items: json,
+        })
+      });
+  }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+
+    const { isLoaded, items } = this.state;
+
+    if(!isLoaded) {
+      return <div>Loading...</div>;
+    }
+    else{
+      return (
+        <div className="App">
+            Table
+        </div>
+        
+      );
+    }
+
+    
   }
 }
 
